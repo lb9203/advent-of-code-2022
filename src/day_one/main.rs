83feb3eccb55781d9532cc45ac08713fe887a498
lb9203 <lb9230@student.uni-lj.si>
2022-12-03 21:02:17 +0100

    Initial commit and day one solution.

---
+++ src/day_one/main.rs
@@ -0,0 +1,43 @@
+mod input;
+
+use crate::input::get_input;
+use std::num::IntErrorKind::Empty;
+
+fn main() {
+    let input = get_input();
+
+    let calories = top(input, 3);
+
+    println!("The Elf with the most calories has {} calories.\n", calories[0]);
+
+    let sum: i32 = calories.iter().sum();
+
+    println!("The top 3 Elves have a combined {} calories.", sum)
+}
+
+fn top(input: &str, number: usize) -> Vec<i32> {
+    let mut calories: Vec<i32> = vec![];
+    let mut running_count = 0;
+    for line in input.lines() {
+        match line.trim().parse::<i32>() {
+            Ok(n) => {
+                running_count += n
+            }
+            Err(e) => {
+                if e.kind() != &Empty {
+                    panic!("Can't parse number from input.")
+                }
+
+                calories.push(running_count);
+
+                running_count = 0
+            }
+        }
+    }
+
+    calories.sort();
+    calories.reverse();
+    calories.truncate(number);
+
+    return calories
+}
